7b382cb29035c2a752bb2d9318569221ad1a840c
김택윤 <gw9122@naver.com>
2022-08-20 19:37:21 +0900

    model 설계 완료

---
+++ src/main/java/com/covenant/springbootmysql/model/Author.java
@@ -0,0 +1,24 @@
+package com.covenant.springbootmysql.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Book> books;
+}
